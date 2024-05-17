d2d97040c5ed4527fcea702145e3bf4c59285ed3
MIRIAM_ALEXA_ZAMORA_DOMINGUEZ <a.21308051280559@cbtis128.edu.mx>
2024-05-17 21:01:26 +0000

    Actualizando

---
+++ lib/tab/donut_tab.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+import '../util/donut_tile.dart';
+
+class DonutTab extends StatelessWidget {
+  // list of donuts
+  List donutsOnSale = [
+    // [ donutFlavor, donutPrice, donutColor, imageName ]
+    ["Nike pick", "900", Colors.blue, "assets/images/avatar1.jpg"],
+    ["Jordan 1", "300", Colors.red, "assets/images/avatar2.jpg"],
+    ["Jordan Air", "200", Colors.purple, "assets/images/avatar3.jpg"],
+    ["Air force", "200", Colors.brown, "assets/images/avatar4.jpg"],
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      itemCount: donutsOnSale.length,
+      padding: EdgeInsets.all(12),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 1 / 1.5,
+      ),
+      itemBuilder: (context, index) {
+        return DonutTile(
+          donutFlavor: donutsOnSale[index][0],
+          donutPrice: donutsOnSale[index][1],
+          donutColor: donutsOnSale[index][2],
+          imageName: donutsOnSale[index][3],
+        );
+      },
+    );
+  }
+}
